70df01c661a6493bb5f946be2e64c4a1955ef35f
alexxcollins <48695916+alexxcollins@users.noreply.github.com>
2022-05-05 15:41:44 +0100

    minor bug fix on utils.py

--- code/utils.py
+++ code/utils.py
@@ -124,7 +124,7 @@ def train(encoder, decoder, criterion, optimizer, train_loader, val_loader, tota
             val_epoch_loss += loss.item()
             if i % print_every == 0:
                 state = "Epoch: {:15} || Step: {:15} || Average Training Loss: {:.4f}".format('[{:d}/{:d}]'.format(epoch,total_epoch),
-                                                                                              '[{:d}/{:d}]'.formati,len(val_loader),
+                                                                                              '[{:d}/{:d}]'.format(i,len(val_loader)),
                                                                                                val_epoch_loss / (i + 1))
                 print(state)
                 file.write(state+'\n')
